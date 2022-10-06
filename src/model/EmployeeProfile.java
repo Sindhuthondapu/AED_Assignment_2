faf51838bdc3b0b8659c6d344e42fb620cfb011f
Sindhuthondapu <thondapu.sa@northeaster.edu>
2022-10-06 15:54:22 +0530

    Initial commit

---
+++ src/model/EmployeeProfile.java
@@ -0,0 +1,118 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author lenovo pc
+ */
+public class EmployeeProfile {
+    private String name;
+    private String employeeID;
+    private int age;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String teamInfo;
+    private String positionTitle;
+    private int cellPhone;
+    private String email;
+    private String profilePhoto;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInfo() {
+        return teamInfo;
+    }
+
+    public void setTeamInfo(String teamInfo) {
+        this.teamInfo = teamInfo;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public int getCellPhone() {
+        return cellPhone;
+    }
+
+    public void setCellPhone(int cellPhone) {
+        this.cellPhone = cellPhone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+           
+    @Override
+    public String toString(){
+        return name;
+    }
+    
+    
+}
---
+++ src/model/EmployeeProfileHistory.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lenovo pc
+ */
+public class EmployeeProfileHistory {
+    
+    private ArrayList<EmployeeProfile> history; 
+    
+    public EmployeeProfileHistory(){
+        this.history = new ArrayList<EmployeeProfile>(); 
+    }
+
+    public ArrayList<EmployeeProfile> getHistory() {
+        return history;
+    }
+
+    public void setHistory(ArrayList<EmployeeProfile> history) {
+        this.history = history;
+    }
+    
+    public EmployeeProfile addNewProfiles(){
+        
+        EmployeeProfile newProfile = new EmployeeProfile();
+        history.add(newProfile);
+        return newProfile;
+        
+    }
+    
+    public void deleteProfile(EmployeeProfile ps){
+        history.remove(ps);
+    }
+
+    
+    
+}
